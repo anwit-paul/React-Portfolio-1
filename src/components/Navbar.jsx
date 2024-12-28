82f7460b7945cb05b20dd15ae8c7a077d77fd58b
anwit-paul <74483986+anwit-paul@users.noreply.github.com>
2024-12-28 20:27:38 +0530

    fix: logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import logo from '../assets/kevinRushLogo.png'
+import logo from '../assets/AP-removebg-preview.png'
 import { FaLinkedin } from 'react-icons/fa'
 
 const Navbar = () => {
     return (
         <nav className='mb-2 flex intems-center justify-between py-6'>
-            <div className='flex flex-shrink=0 items-center mx-6 w-10 h-10'>
+            <div className='flex flex-shrink-0 items-center mx-6 w-20 h-20'>
                 <img src={logo} alt="logo" />
             </div>
             <div className='flex items-center text-3xl mx-8'>
