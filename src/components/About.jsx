15db14782664306301a8913ef00839461b96629e
anwit-paul <74483986+anwit-paul@users.noreply.github.com>
2024-12-29 16:45:19 +0530

    fix: photos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import aboutImg from '../assets/about.jpg'
+import aboutImg from '../assets/coder2.jpg'
 import { ABOUT_TEXT } from '../constants'
 import { motion } from 'framer-motion'
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import logo from '../assets/AP-removebg-preview.png'
 import { FaLinkedin } from 'react-icons/fa'
 
+const openLinkedin = () => {
+    window.open('https://www.linkedin.com/in/anwit-paul-b9b6a41b4/', '_blank', 'width=800,height=800')
+}
+
 const Navbar = () => {
     return (
         <nav className='mb-2 flex intems-center justify-between py-6'>
             <div className='flex flex-shrink-0 items-center mx-6 w-20 h-20'>
                 <img src={logo} alt="logo" />
             </div>
-            <div className='flex items-center text-3xl mx-8'>
+            <div className='flex items-center text-3xl mx-8' onClick={openLinkedin}>
                 <FaLinkedin />
             </div>
         </nav>
